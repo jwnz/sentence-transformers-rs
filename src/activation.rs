dfbc8eb8548e01c2f7f5c01bc3922f56274e7b8d
jwnz <tkjones93@gmail.com>
2025-09-19 11:17:44 +0900

    add identity activation

--- src/activation.rs
+++ src/activation.rs
@@ -8,11 +8,16 @@ pub enum Activation {
     #[serde(alias = "Tanh")]
     #[serde(alias = "torch.nn.modules.activation.Tanh")]
     Tanh,
+
+    #[serde(alias = "Identity")]
+    #[serde(alias = "torch.nn.modules.linear.Identity")]
+    Identity,
 }
 impl Activation {
     pub fn forward(&self, xs: &Tensor) -> Result<Tensor, ActivationError> {
         let xs = match self {
             Activation::Tanh => xs.tanh()?,
+            Activation::Identity => xs.clone(),
         };
 
         Ok(xs)
